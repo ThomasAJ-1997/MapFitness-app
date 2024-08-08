8622a6ccb47bf533bedca8ec2e3294af4c80e78a
Thomas Jones <Thomasajones@hotmail.co.uk>
2024-08-08 12:18:35 +0100

    map features

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + "").slice(-10); // Often we use libaries to create IDs.
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat, lng]
@@ -20,6 +21,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+
+  clicks() {
+    this.clicks++;
   }
 }
 
@@ -69,6 +74,7 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
@@ -76,6 +82,7 @@ class App {
     this._getPosition();
     form.addEventListener("submit", this._newWorkout.bind(this));
     inputType.addEventListener("change", this._toggleElevationField);
+    containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -96,7 +103,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
     L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
@@ -167,7 +174,7 @@ class App {
         !positiveNumbers(distance, duration)
       )
         return alert("Inputs have to be positive numbers");
-      workout = new Cycling([lat, lng], distance, duration);
+      workout = new Cycling([lat, lng], distance, duration, elevation);
     }
     // Add new object to the workout array
     this.#workouts.push(workout);
@@ -251,8 +258,30 @@ class App {
 
     form.insertAdjacentHTML("afterend", html);
   }
-}
 
+  _moveToPopup(e) {
+    if (!this.#map) return;
+
+    const workoutEl = e.target.closest(".workout");
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      (work) => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // Using the public interface
+    workout.click();
+  }
+}
+///////////////////////////////////////////////////////////////////////
 const app = new App();
 
 ////////////////////////////////////////////////////////////////////////
